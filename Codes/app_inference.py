779f0efc31dc34158d747b42e59b4228924dc736
khaphieutr <intern.phieu@primelabo.com.vn>
2019-07-22 16:46:17 +0700

    update performance

--- Codes/app_inference.py
+++ Codes/app_inference.py
@@ -291,11 +291,12 @@ with tf.Session(config=config) as sess:
                 #print ("frames out:", frame_out)
 
                 #frame = cv2.imread(img_path)
+                '''
                 frame = frames_list[it]
                 frame = np.uint8 (frame)
                 print ("frame_shape:", frame.shape)
                 frame = cv2.resize(frame, (W, H))
-
+                '''
                 #H, W = frame.shape[:2]
                 
                 #l_val = np.load(mask_path + mask_list[it])               
